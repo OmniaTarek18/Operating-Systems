be195c177e57ee5e4be1379da16c911ac3bb2eeb
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-04-11 14:30:41 +0200

    Update caltrain.c

--- Labs/lab3/lab3_starter_code/caltrain.c
+++ Labs/lab3/lab3_starter_code/caltrain.c
@@ -1,28 +1,56 @@
 #include <pthread.h>
 #include "caltrain.h"
 
-
-
-void
-station_init(struct station *station)
+void station_init(struct station *station)
 {
-	// FILL ME IN
+	station->numOfEmptySeats = 0;
+	station->numOfWaitingPeople = 0;
+	station->numOfPeopleWalkingOnTrain = 0;
+	pthread_mutex_init(&station->mutex, NULL);
+	pthread_cond_init(&station->train_is_maybe_ready_to_leave, NULL);
+	pthread_cond_init(&station->seats_are_available, NULL);
 }
 
-void
-station_load_train(struct station *station, int count)
+void station_load_train(struct station *station, int count)
 {
-	// FILL ME IN
+
+	pthread_mutex_lock(&station->mutex);
+	station->numOfEmptySeats = count;
+
+	while (station->numOfWaitingPeople !=0 && station->numOfEmptySeats != 0)
+	{
+		pthread_cond_broadcast(&station->seats_are_available);
+		pthread_cond_wait(&station->train_is_maybe_ready_to_leave, &station->mutex);
+	}
+
+	pthread_mutex_unlock(&station->mutex);
 }
 
-void
-station_wait_for_train(struct station *station)
+void station_wait_for_train(struct station *station)
 {
-	// FILL ME IN
+	pthread_mutex_lock(&station->mutex);
+	station->numOfWaitingPeople++;
+	while (station->numOfEmptySeats == station->numOfPeopleWalkingOnTrain)
+	{
+		pthread_cond_wait(&station->seats_are_available, &station->mutex);
+	}
+	station->numOfWaitingPeople--;
+	station->numOfPeopleWalkingOnTrain++;
+
+	pthread_mutex_unlock(&station->mutex);
 }
 
-void
-station_on_board(struct station *station)
+void station_on_board(struct station *station)
 {
-	// FILL ME IN
+    pthread_mutex_lock(&station->mutex);
+
+    station->numOfPeopleWalkingOnTrain--;
+	station->numOfEmptySeats--;
+
+	if (station->numOfPeopleWalkingOnTrain == 0)
+	{
+		pthread_cond_signal(&station->train_is_maybe_ready_to_leave);
+	}
+
+    pthread_mutex_unlock(&station->mutex);
 }
