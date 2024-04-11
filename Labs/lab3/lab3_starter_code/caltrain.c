c107bbf8264d8b097a9060c7ea65611fe3f0d2ae
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-04-11 15:58:14 +0200

    Update caltrain.c

--- Labs/lab3/lab3_starter_code/caltrain.c
+++ Labs/lab3/lab3_starter_code/caltrain.c
@@ -17,12 +17,12 @@ void station_load_train(struct station *station, int count)
 	pthread_mutex_lock(&station->mutex);
 	station->numOfEmptySeats = count;
 
-	while (station->numOfWaitingPeople != 0 && station->numOfEmptySeats != 0)
+	while(station->numOfWaitingPeople !=0 && station->numOfEmptySeats != 0)
 	{
 		pthread_cond_broadcast(&station->seats_are_available);
 		pthread_cond_wait(&station->train_is_maybe_ready_to_leave, &station->mutex);
 	}
-	station->numOfEmptySeats = 0;	
+	station->numOfEmptySeats = 0;
 	pthread_mutex_unlock(&station->mutex);
 }
 
@@ -30,12 +30,13 @@ void station_wait_for_train(struct station *station)
 {
 	pthread_mutex_lock(&station->mutex);
 	station->numOfWaitingPeople++;
-	while (station->numOfEmptySeats == station->numOfPeopleWalkingOnTrain)
+	while (station->numOfEmptySeats == 0)
 	{
 		pthread_cond_wait(&station->seats_are_available, &station->mutex);
 	}
 	station->numOfWaitingPeople--;
 	station->numOfPeopleWalkingOnTrain++;
+	station->numOfEmptySeats--;
 
 	pthread_mutex_unlock(&station->mutex);
 }
@@ -43,11 +44,9 @@ void station_wait_for_train(struct station *station)
 void station_on_board(struct station *station)
 {
     pthread_mutex_lock(&station->mutex);
-
     station->numOfPeopleWalkingOnTrain--;
-	station->numOfEmptySeats--;
 
-	if (station->numOfEmptySeats == 0 || (station->numOfPeopleWalkingOnTrain == 0 && station->numOfWaitingPeople==0))
+	if (station->numOfPeopleWalkingOnTrain == 0 && (station->numOfWaitingPeople ==0 || station->numOfEmptySeats == 0))
 	{
 		pthread_cond_signal(&station->train_is_maybe_ready_to_leave);
 	}
